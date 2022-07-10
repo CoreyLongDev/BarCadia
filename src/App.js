073b8c09437befd05acf6e0c2d89ffb9c08cbaba
CoreyLongDev <coreylongdev@gmail.com>
2022-07-10 10:16:10 -0400

    added basic route with 404. deleted a few more extras from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import NotFoundPage from './Pages/NotFoundPage';
 
 function App() {
   return (
-    <h1>hello world testing autosave</h1>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFoundPage = () => {
+    return(
+        <h1>404: No Such Page Brah</h1>
+    )
+}
+
+export default NotFoundPage
