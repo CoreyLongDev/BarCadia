5e405c148e44c751f8a42d9b0f600e6d589087df
CoreyLongDev <coreylongdev@gmail.com>
2022-07-11 21:36:13 -0400

    all pages skeletized and routed via app.js tested good. time to actually design some pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Play from './Pages/Play';
+import Drink from './Pages/Drink';
+import Events from './Pages/Events';
 import About from './Pages/About';
+import Login from './Pages/Login';
 import NotFoundPage from './Pages/NotFoundPage';
+import Member from './Pages/Member';
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/play' element={<Play />} />
+          <Route path='/drink' element={<Drink />} />
+          <Route path='/events' element={<Events />} />
           <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/member' element={<Member />} />
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
     </Router>
---
+++ src/Pages/Member.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Member = () => {
+    return(
+        <h1>you logged in brah</h1>
+    )
+}
+
+
+
+
+
+
+export default Member;
