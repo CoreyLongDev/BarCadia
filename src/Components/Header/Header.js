388a61aeef347b72e0c7ad36fe60830baf2605e2
CoreyLongDev <coreylongdev@gmail.com>
2022-07-11 22:38:00 -0400

    navbar init inside header. will utilize together throughout site no need for seperate components.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
     return(
         <>
         <h1 id="header">Barcadia</h1>
-        <ul>
-            <li>Play</li>
-            <li>Drink</li>
-            <li>Events</li>
-            <li>About</li>
+        <ul className="navBar">
+            <li><a href="./Play">Play</a></li>
+            <li><a href="./Drink">Drink</a></li>
+            <li><a href="./Events">Events</a></li>
+            <li><a href="./About">About Us</a></li>
         </ul>
         </>
     )
