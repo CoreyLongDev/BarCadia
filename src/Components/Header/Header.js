668349f14fc8d496c62516bbd0d5159292b7f3d1
CoreyLongDev <coreylongdev@gmail.com>
2022-07-13 22:56:32 -0400

    header 80% done. just need to modify logo a bit and throw it into public folder and all set.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
         <>
         <h1 id="header">Barcadia</h1>
 
-        <Navbar bg='light' variant='light'>
-            <Container>
+        <Navbar bg='info' variant='dark'>
+            <Container className="Navi">
             <Nav.Link href="./Home">Home</Nav.Link>
             <Nav.Link href="./Play">Play</Nav.Link>
             <Nav.Link href="./Reload">Reload</Nav.Link>
