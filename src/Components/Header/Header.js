1ae26cb0330b064341975621451b0b2960865344
CoreyLongDev <coreylongdev@gmail.com>
2022-07-13 21:20:33 -0400

    testing bootstrap and MDB-UI

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import { NavBar } from mdb-react-ui-kit
+import { Tab } from 'mdb-react-ui-kit';
 
 const Header = () => {
     return(
         <>
-        <h1 id="header">Barcadia</h1>
-        <ul className="navBar">
-            <li><a href="./Play">Play</a></li>
-            <li><a href="./Drink">Drink</a></li>
-            <li><a href="./Events">Events</a></li>
-            <li><a href="./About">About Us</a></li>
-        </ul>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Library</a></li>
+        <li class="breadcrumb-item active" aria-current="page"><a href="#">Data</a></li>
+      </ol>
+    </nav>
+  </div>
+</nav>
         </>
     )
 }
