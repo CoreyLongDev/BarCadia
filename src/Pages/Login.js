3e7186e9cc038fe0f488fed3c1caf35ffa208781
PatriotxZero <Coreylong90@icloud.com>
2022-07-16 10:57:48 -0400

    changes to login page. on standby until passport.js init

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,23 +4,16 @@ import { Button, Form } from 'react-bootstrap';
 function MemberLogin() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+      <Form.Group className="mb-1" controlId="memberEmail">
+        <Form.Label>Email address:</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
+      <Form.Group className="mb-1" controlId="memberPassword">
+        <Form.Label>Password:</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        Log In
       </Button>
     </Form>
   );
