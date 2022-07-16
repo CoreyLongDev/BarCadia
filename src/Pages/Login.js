c2b39fef16cf6bddc5710d74360f8072542cdfee
PatriotxZero <Coreylong90@icloud.com>
2022-07-16 11:29:31 -0400

    closing out front end for now to work on back.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 
 function MemberLogin() {
   return (
-    <Form>
-      <Form.Group className="mb-1" controlId="memberEmail">
-        <Form.Label>Email address:</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-1" controlId="memberPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Log In
-      </Button>
-    </Form>
+    <Container id='logger'>
+        <Form>
+            <Form.Group className="mb-1" controlId="memberEmail">
+            <Form.Label>Email address:</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+            <Form.Group className="mb-1" controlId="memberPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+            <Button variant="primary" type="submit">
+                Log In
+            </Button>
+        </Form>
+    </Container>
   );
 }
 
