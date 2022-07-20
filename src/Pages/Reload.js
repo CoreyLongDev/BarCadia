c9601c6f0fd344c36c53bf47bac5a77ea53d2906
CoreylongDev <Coreylong90@icloud.com>
2022-07-20 08:36:10 -0400

    reload page styled. additional items added in respective SQL db.

--- src/Pages/Reload.js
+++ src/Pages/Reload.js
@@ -2,38 +2,32 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const Reload = () => {
+  const [beers, setBeers] = useState([]);
 
-    const [beers, setBeers] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:4000/api/v1/getBeers")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBeers(data);
+      });
+  }, []);
 
-useEffect(() => {
-    fetch('http://localhost:4000/api/v1/getBeers')
-    .then(res => res.json())
-
-    .then(
-        data => {
-            console.log(data)
-            setBeers(data)
-        }
-    )
-},[])
-
-
-    return(
-        <>
-        <p>Heu is this showing</p>
-       { beers.map((beer, index) => {
-            return (
-            
-                <p key={index}>{beer.name}</p>
-            )
-        })}
-        </>
-    )
-}
-
-
-
-
-
+  return (
+    <>
+      
+      {beers.map((beer, index) => {
+        return <div className="cards" key={index}>
+        {beer.name}  &nbsp;|&nbsp;
+        {beer.style} &nbsp;|&nbsp;
+        {beer.abr}%  &nbsp;|&nbsp; <span style={{color: 'green'}}>$ </span>
+        {beer.price}
+        
+        </div>;
+        
+      })}
+    </>
+  );
+};
 
 export default Reload;
