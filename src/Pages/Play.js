fec14209807f2f138b6e76b32c85fd9decbf0697
CoreylongDev <Coreylong90@icloud.com>
2022-07-22 17:19:26 -0400

    clean slate after failed deployment

--- src/Pages/Play.js
+++ src/Pages/Play.js
@@ -1,14 +1,50 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import Controller from '../Images/multiplayerIcon.png'
 
-const Play = () => {
-    return(
-        <h1>current arcade list from SQL here</h1>
-    )
-}
+const Reload = () => {
+  const [games, setGames] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:4000/api/v1/getGames")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setGames(data);
+      });
+  }, []);
 
+  return (
+    <>
+    <h1 className="pageTitler">Current Games List</h1>
+    <div id="cardWrapper">
+      {games.map((game, index) => {
+        return (
+          <Card className="cards" key={index}>
+            <Card.Img
+              variant="top"
+              src={game.img}
+              style={{ maxHeight: "300px" }}
+            />
+            <Card.Body style={{minWidth: '300px', maxWidth: '300px',  maxHeight: "250px" }}>
+              <Card.Title>{game.name}</Card.Title>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>{game.type}</ListGroup.Item>
+                <ListGroup.Item>
+                  <span style={{ color: "orange" }}><img id="controller" src={Controller} alt="game controller" />{game.maxplayers}</span>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+            <Card.Body>
+              <Card.Link id="orderBeer" href="grgre"> &nbsp;&nbsp;ORDER&nbsp;&nbsp; </Card.Link>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
+    </>
+  );
+};
 
-
-
-
-export default Play;
+export default Reload;
