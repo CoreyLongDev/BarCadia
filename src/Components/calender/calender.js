166a6a3d9d28cd708cf24d8c65f6dd88dd68a548
CoreylongDev <Coreylong90@icloud.com>
2022-07-23 10:35:43 -0400

    small calender changes

--- src/Components/calender/calender.js
+++ src/Components/calender/calender.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import './calender.css'
 
 export default class DemoApp extends React.Component {
   render() {
     return (
       <FullCalendar
-        plugins={[ dayGridPlugin ]}
+        plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
+        weekends={true}
+        events={[
+          { title: "Presentation Day", date: "2022-07-23" },
+          { title: "MVC Tournament", date: "2022-07-12" },
+          { title: "$2 Drafts", date: "2022-07-15"},
+          { title: "First Friday All-Nighter", date: "2022-07-01"}
+        ]}
       />
-    )
+    );
   }
 }
