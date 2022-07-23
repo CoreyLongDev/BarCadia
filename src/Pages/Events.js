2e336e83b478d1aba6484fa1d54a7c6b78935337
CoreylongDev <Coreylong90@icloud.com>
2022-07-22 21:51:49 -0400

    events page note added for clients & contributors to review

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -3,7 +3,10 @@ import ComingSoon from "../Components/ComingSoon/ComingSoon";
 
 const Events = () => {
     return(
+      <>
       <ComingSoon />
+      <p className="babble">A Event or calander system to give your customers a glimpse into things happening around the barcade.</p>
+      </>
     )
 }
 
